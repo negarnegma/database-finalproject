89189740abbc1a4ba175786f80ac4395604cbdb2
negarnegma <negarzist@gmail.com>
2020-01-30 03:08:35 +0330

    sql

--- exec queries/views.sql
+++ exec queries/views.sql
@@ -1,4 +1,4 @@
-create view registering_users_view AS
-select nasional_number, fullname, address, pass, salt
-from "user",
-     akshaye_tabriz;
+create or replace view registering_users_view AS
+select "user".id, nasional_number, fullname, address, pass, salt
+from "user"
+         left join akshaye_tabriz a on "user".password_id = a.id;
