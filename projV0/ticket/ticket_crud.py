57e1d37c8779f33b1318bdaf3f7337caee4f150c
negarnegma <negarzist@gmail.com>
2020-01-29 20:10:59 +0330

    ticket and ui

--- projV0/ticket/ticket_crud.py
+++ projV0/ticket/ticket_crud.py
@@ -42,16 +42,23 @@ def get_all_user_tickets(ui, user_id):
         " where  owner_id = %s and first_ticket_id is null ",
         (user_id,))
     rows = cur.fetchall()
+
+    if rows is None:
+        return
+
     for x in rows:
         p1 = Ticket(x[0], x[1], x[2], x[3], x[4], 0, 0)
         tickets.append(p1)
+
     con.close()
     # print all_user_tickets
     ui.textEdit_5.setText("")
-    ui.textEdit_5.insertPlainText("id   date                 content")
+    ui.textEdit_5.insertPlainText("  id   date                 content")
     for ticket in tickets:
-        ui.textEdit_5.insertPlainText("\n%5.5s%21.21s%s" %
-                                      (ticket.ticket_id, ticket.answer_date, ticket.question))
+        ui.textEdit_5.insertPlainText("\n  %2.2s   %21.21s%s" %
+                                      (ticket.ticket_id, ticket.c_date, ticket.content))
+    # else:
+    #     ui.textEdit_5.setText("")
 
 
 def get_all_answers_for_a_ticket(ui, user_id, ticket_id):
@@ -65,7 +72,12 @@ def get_all_answers_for_a_ticket(ui, user_id, ticket_id):
                 " where id = %s",
                 (ticket_id,))
     row = cur.fetchone()
+
+    if row is None:
+        return
+
     p1 = Ticket(row[0], row[1], row[2], row[3], row[4], 0, 0)
+    tickets.append(p1)
 
     if p1.owner_id != user_id:
         ui.textEdit_4.setText("access denied!")
@@ -75,19 +87,26 @@ def get_all_answers_for_a_ticket(ui, user_id, ticket_id):
                 " where first_ticket_id = %s",
                 (ticket_id,))
     rows = cur.fetchall()
+
+    if rows is None:
+        return
+
     for row in rows:
         p1 = Ticket(row[0], row[1], row[2], row[3], row[4], ticket_id, row[5])
         tickets.append(p1)
 
     con.close()
 
-    sorted(key=lambda x: x.order)
+    # sorted(key=lambda x: x.order)
 
     # print all_answers_for_a_ticket
-    ui.textEdit_4.insertPlainText("id   date                 content")
+    ui.textEdit_4.setText("")
+    ui.textEdit_4.insertPlainText("  id   date                 content")
     for ticket in tickets:
-        ui.textEdit_4.insertPlainText("\n%5.5s%21.21s%s" %
-                                      (ticket.ticket_id, ticket.answer_date, ticket.question))
+        ui.textEdit_4.insertPlainText("\n  %2.2s   %21.21s%s" %
+                                      (ticket.ticket_id, ticket.c_date, ticket.content))
+    # else:
+    #     ui.textEdit_4.setText("")
 
 
 if __name__ == "__main__":
